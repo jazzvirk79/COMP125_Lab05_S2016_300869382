f5ea26a90b2d6bf7d8292ddbd955e5cc05325df0
jazzvirk79 <jagdeep.virk5@gmail.com>
2016-08-06 14:15:48 -0400

    lab done

---
+++ Scripts/app.ts
@@ -0,0 +1,48 @@
+/// <reference path="objects/label.ts"/>
+
+/**
+ * FileName: app.js
+ * 
+ * @author: Jagdeep Virk
+ * 
+ * @date August 6, 2016
+ * 
+ * StudentID: 300869382
+ * 
+ * @description This file is the main javascript file for the web site
+ */
+
+// IIFE - Immediately Invoked Function Expression
+(function () {
+    "use strict";
+
+    var canvas:HTMLElement;
+    var stage:createjs.Stage;
+    var helloLabel:objects.Label;
+
+    // app entry function
+    function init():void {
+
+        canvas = document.getElementById("canvas");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 frames per second
+        createjs.Ticker.on("tick", gameLoop); // call gameLoop every frame
+        main();
+    }
+
+    function gameLoop():void {
+        helloLabel.rotation -= 5;
+        stage.update(); // refresh the stage container
+    }
+
+
+    function main():void {
+        helloLabel = new objects.Label("Jagdeep", "40px Consolas","#000000", 160, 160, true);
+        stage.addChild(helloLabel);
+    }
+
+    // call init funciton when window finishes loading
+    window.addEventListener("load", init);
+
+
+})();
